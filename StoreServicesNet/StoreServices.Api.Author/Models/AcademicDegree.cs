204cfa21ac13357b86cd47e3a949c68c61ccfe3d
Randhyll <randhallr@portobellosouth.com>
2021-11-22 01:54:45 -0300

    Add Models Microservices

---
+++ StoreServicesNet/StoreServices.Api.Author/Models/AcademicDegree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StoreServices.Api.Author.Models
+{
+    public class AcademicDegree
+    {
+        public Int64 AcademicDegreeID { get; set; }
+        public String NameGraduate { get; set; }
+        public String AcademicCenter{get; set;}
+        public DateTime? DateDegree { get; set; }
+        public Int64? BookAuthorID { get; set; }
+        public BookAuthor BookAuthor { get; set; }
+        public String AcademicDegreeGuid { get; set; }
+    }
+}
---
+++ StoreServicesNet/StoreServices.Api.Author/Models/BookAuthor.cs
@@ -0,0 +1,17 @@
+﻿namespace StoreServices.Api.Author.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class BookAuthor
+    {
+        public Int64? BookAuthorID { get; set; }
+        public String NameAuthor { get; set; }
+        public String LastNameAuthor { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public ICollection<AcademicDegree> AcademicDegreeList { get; set; }
+        public String BookAuthorGuid { get; set; }
+    }
+}
