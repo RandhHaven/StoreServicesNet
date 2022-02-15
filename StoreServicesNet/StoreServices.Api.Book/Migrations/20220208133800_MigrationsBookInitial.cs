7a9c99f71e2b1ff357928dadb273d6e0223c9802
Randhall Rangel <rrangel@cetapappgmail255.onmicrosoft.com>
2022-02-15 13:36:08 -0300

    Add Migrations

---
+++ StoreServicesNet/StoreServices.Api.Book/Migrations/20220208133800_MigrationsBookInitial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StoreServices.Api.Book.Migrations
+{
+    public partial class MigrationsBookInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
